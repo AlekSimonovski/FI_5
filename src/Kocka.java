c31ba67744392865a4bfeebb241cb0dc8090dc1c
unknown <alek.simonovski02@gmail.com>
2023-03-27 09:51:26 +0200

    Added comments to code.

--- src/Kocka.java
+++ src/Kocka.java
@@ -7,7 +7,7 @@ public class Kocka {
 		System.out.println("Programata presmetuva plostina i perimetar na kocka");
 
 		System.out.println("Vnesete strana A: ");
-
+		/// Takes user input for A value
 		a = tastatura.nextDouble();
 		
 		perimetar = 4*a;
